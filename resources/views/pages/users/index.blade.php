5ddac44c7555f1da47f4dafc95229f3e95668e57
Yash Pawar <yashdvn@gmail.com>
2022-03-24 11:29:28 +0530

    fixed users list index page

--- resources/views/pages/users/index.blade.php
+++ resources/views/pages/users/index.blade.php
@@ -2,6 +2,7 @@
 @section('title', "User's List")
 
 @section('content')
+<link href="{{asset('/backend/css/bootstrap.min.css')}}" id="bootstrap-style" rel="stylesheet" type="text/css" />
 <div class="page-content">
     <div class="container-fluid">
     <!-- start page title -->
@@ -57,31 +58,16 @@
 
 @section('header')
 <!-- DataTables -->
-<link href="/backend/libs/datatables.net-bs4/css/dataTables.bootstrap4.min.css" rel="stylesheet" type="text/css" />
-<link href="/backend/libs/datatables.net-buttons-bs4/css/buttons.bootstrap4.min.css" rel="stylesheet" type="text/css" />
-
- <!-- Responsive datatable examples -->
-<link href="/backend/libs/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css" rel="stylesheet" type="text/css" />     
+<link href="{{asset('/backend/libs/datatables.net-bs4/css/dataTables.bootstrap4.min.css')}}" rel="stylesheet" type="text/css" />
 @endsection
 
 @section('footer')
 <!-- Required datatable js -->
-<script src="/backend/libs/datatables.net/js/jquery.dataTables.min.js"></script>
-<script src="/backend/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js"></script>
+<script src="{{asset('/backend/libs/datatables.net/js/jquery.dataTables.min.js')}}"></script>
+<script src="{{asset('/backend/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js')}}"></script>
 <!-- Buttons examples -->
-<script src="/backend/libs/datatables.net-buttons/js/dataTables.buttons.min.js"></script>
-<script src="/backend/libs/datatables.net-buttons-bs4/js/buttons.bootstrap4.min.js"></script>
-<script src="/backend/libs/jszip/jszip.min.js"></script>
-<script src="/backend/libs/pdfmake/build/pdfmake.min.js"></script>
-<script src="/backend/libs/pdfmake/build/vfs_fonts.js"></script>
-<script src="/backend/libs/datatables.net-buttons/js/buttons.html5.min.js"></script>
-<script src="/backend/libs/datatables.net-buttons/js/buttons.print.min.js"></script>
-<script src="/backend/libs/datatables.net-buttons/js/buttons.colVis.min.js"></script>
-        
-<!-- Responsive examples -->
-<script src="/backend/libs/datatables.net-responsive/js/dataTables.responsive.min.js"></script>
-<script src="/backend/libs/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js"></script>
-
-<!-- Datatable init js -->
-<script src="/backend/js/pages/datatables.init.js"></script>  
+<script src="{{asset('/backend/libs/datatables.net-buttons/js/dataTables.buttons.min.js')}}"></script>
+<script src="{{asset('/backend/js/pages/datatables.init.js')}}"></script>  
+<!-- Sweet Alerts js -->
+<script src="{{asset('/backend/libs/sweetalert2/sweetalert2.min.js')}}"></script>
 @endsection
